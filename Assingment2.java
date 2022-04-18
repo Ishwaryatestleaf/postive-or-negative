c39f1c08c421ff17672726d5f34a0c4ce3c55bff
Ishwaryatestleaf <103950121+Ishwaryatestleaf@users.noreply.github.com>
2022-04-18 21:16:37 +0530

    Add files via upload

---
+++ Assingment2.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Assingment2 {
+	public static void main(String[] args) {
+		int num=35;
+		if(num>0) {
+		System.out.println("Number is positive");
+			
+		}
+		if(num<0) {
+		System.out.println("Number is negative");	
+		
+		}
+		if(num==0) {
+		System.out.println("Number is neither positive nor negative");
+		}
+		
+	}
+
+}
